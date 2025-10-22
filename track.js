55022191ba56f0d6d352bc4eeab85c241b45ae59
Ace-ix <anuragh.s@atriauniversity.edu.in>
2025-10-22 14:54:57 +0530

    add summary

--- track.js
+++ track.js
@@ -9,22 +9,28 @@ function addItem() {
         alert("Please fill all the fields.");
         return;
     }
-    var expense = {
-        Date: Date.value,
-        itemName: itemName.value,
-        amount: amount.value,
-        Type: Type.value
-    }
+    var expense = [Date.value, itemName.value, amount.value, Type.value];
     expenses.push(expense);
-
     var col = Type.value === "cashIn" ? "green" : "red";
     var list = document.getElementById('tableBody');
     var element = document.createElement('tr');
     element.style.color = col;
     element.innerHTML = `<td>${Date.value}</td><td>${itemName.value}</td><td>${'₹'+amount.value}</td><td>${Type.value}</td><td><button id='remove'onclick="this.parentElement.parentElement.remove()">remove</button></td>`;
     list.appendChild(element);
-    document.getElementById('expenseForm').reset();
+    document.getElementById('expenseForm').reset();  
 
+    var totalCashIn = 0;
+    var totalCashOut = 0;
+    for (i in expenses) {
+        if (expenses[i][3] === "cashIn") {
+            totalCashIn += parseFloat(expenses[i][2]);}
+        else {
+            totalCashOut += parseFloat(expenses[i][2]);}
+    }
+    var totalcashin = document.getElementById('cashin');
+    var balance = document.getElementById('bal');
+    var totalcashout = document.getElementById('cashout');
+    totalcashin.innerText = "Total Cash In: "+'₹ ' + totalCashIn;
+    totalcashout.innerText = "Total Cash Out: "+'₹ ' + totalCashOut;
+    balance.innerText = "Balance: "+'₹ ' + (totalCashIn - totalCashOut);
 }
-localStorage.setItem('expenses', JSON.stringify(expenses));
-console.log(localStorage.getItem('expenses'));
